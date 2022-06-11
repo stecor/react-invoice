cb932be7aee38a82af6d6f9cd1b99c195a598fdd
Stefano Corra <stefano.corra@hotmail.com>
2022-06-10 23:52:51 -0400

    table commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,51 +1,123 @@
 import React, { useState } from 'react'
 import { Table, Button } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faThumbsUp,
+  faThumbsDown,
+  faImage,
+  faMoneyCheckAlt,
+  faSearchDollar,
+} from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
+  const initialState = [
+    {
+      id: '100',
+      Vendor: 'Firestone',
+      Amount: '$12,000',
+      Invoice: '1135',
+      Date: '03/30/2019',
+    },
+    {
+      id: '200',
+      Vendor: 'Goodyear',
+      Amount: '$21,000',
+      Invoice: '1136',
+      Date: '06/12/2019',
+    },
+    {
+      id: '300',
+      Vendor: 'Firestone',
+      Amount: '$16,000',
+      Invoice: '1137',
+      Date: '05/15/2020',
+    },
+    {
+      id: '400',
+      Vendor: 'Hankook',
+      Amount: '$11,000',
+      Invoice: '1236',
+      Date: '02/25/2020',
+    },
+    {
+      id: '500',
+      Vendor: 'Hankook',
+      Amount: '$8,000',
+      Invoice: '2139',
+      Date: '08/21/2021',
+    },
+  ]
+
   const [state, setState] = useState({
     isLoading: false,
-    invoices: [],
+    invoices: initialState,
   })
 
   const { isLoading, invoices } = state
 
-  const body = [
-    {
-      id: '100',
-      Vendor: 'Hankook',
-      Amount: '$18,000',
-      Date: '08/21/2019',
-    },
-    {
-      id: '100',
-      Vendor: 'Hankook',
-      Amount: '$18,000',
-      Date: '08/21/2019',
-    },
-    {
-      id: '100',
-      Vendor: 'Hankook',
-      Amount: '$18,000',
-      Date: '08/21/2019',
-    },
-    {
-      id: '100',
-      Vendor: 'Hankook',
-      Amount: '$18,000',
-      Date: '08/21/2019',
-    },
-    {
-      id: '100',
-      Vendor: 'Hankook',
-      Amount: '$18,000',
-      Date: '08/21/2019',
-    },
-  ]
+  const remove = (id) => {
+    console.log(id)
+    let updatedInvoices = [...invoices].filter((i) => i.id !== id)
+    setState({
+      invoices: updatedInvoices,
+    })
+  }
+
+  let allInvoices = invoices.map((inv) => {
+    return (
+      <tr key={inv.id}>
+        <td>{inv.Vendor}</td>
+        <td>{inv.Amount}</td>
+        <td>{inv.Invoice}</td>
+        <td>{inv.Date}</td>
+        <td>
+          <Button
+            className='btn btn-lg btn-success'
+            onClick={() => remove(inv.id)}
+          >
+            <FontAwesomeIcon icon={faThumbsUp} /> OK
+          </Button>
+        </td>
+        <td>
+          <Button
+            className='btn btn-lg btn-danger'
+            onClick={() => remove(inv.id)}
+          >
+            <FontAwesomeIcon icon={faThumbsDown} /> Not OK
+          </Button>
+        </td>
+        <td>
+          <Button
+            className='btn btn-lg btn-info'
+            onClick={() => remove(inv.id)}
+          >
+            <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50%
+          </Button>
+        </td>
+        <td>
+          <Button
+            className='btn btn-lg btn-warning'
+            onClick={() => remove(inv.id)}
+          >
+            <FontAwesomeIcon icon={faSearchDollar} /> ??
+          </Button>
+        </td>
+        <td>
+          <Button
+            className='btn btn-lg btn-info'
+            onClick={() => remove(inv.id)}
+          >
+            <FontAwesomeIcon icon={faImage} /> image
+          </Button>
+        </td>
+      </tr>
+    )
+  })
 
   const Load = isLoading ? (
     <div>Loading...</div>
   ) : (
-    <div className='container border border-secondary rounded center'>
+    <div className='container border border-secondary rounded center mt-5'>
       <div className='row'>
         <div className='col-12'>
           <h4>Pending Invoices - The Company</h4>
@@ -55,10 +127,24 @@ const App = () => {
         <div className='.col-xs-12 center text-center'>
           <Table dark responsive striped bordered hover>
             <thead>
-              <th>Vendor</th>
-              <th>Vendor</th>
-              <th>Vendor</th>
+              <tr>
+                <th>Vendor</th>
+                <th>Amount</th>
+                <th>Invoice #</th>
+                <th>Date</th>
+                <th colSpan='4'>Actions</th>
+                <th>Image</th>
+              </tr>
             </thead>
+            <tbody>
+              {invoices.length === 0 ? (
+                <tr>
+                  <td colSpan='9'>All caught up!</td>
+                </tr>
+              ) : (
+                allInvoices
+              )}
+            </tbody>
           </Table>
         </div>
       </div>
